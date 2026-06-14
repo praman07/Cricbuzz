0c82627cfb988b0f3e87e0fc8e314dcc5ca9cd3f
Siddhant Mul <siddhantmul18@gmail.com>
2026-06-14 11:48:07 +0530

    feat(series): add request validation

--- server/src/modules/series/series.route.js
+++ server/src/modules/series/series.route.js
@@ -1,16 +1,26 @@
 import { Router } from "express";
 import SeriesController from "./series.controller.js";
+import { createSeriesSchema, updateSeriesSchema } from "./validators/series.validator.js";
+import validateRequest from "../../shared/middlewares/validateRequest.js";
 
 const router = Router();
 const seriesController = new SeriesController();
 
-router.post("/", seriesController.createSeries.bind(seriesController));
+router.post(
+  "/",
+  validateRequest(createSeriesSchema),
+  seriesController.createSeries.bind(seriesController),
+);
 
 router.get("/", seriesController.getAllSeries.bind(seriesController));
 
 router.get("/:id", seriesController.getSeriesById.bind(seriesController));
 
-router.patch("/:id", seriesController.updateSeries.bind(seriesController));
+router.patch(
+    "/:id",
+    validateRequest(updateSeriesSchema),
+    seriesController.updateSeries.bind(seriesController),
+  );
 
 router.delete("/:id", seriesController.deleteSeries.bind(seriesController));
 
